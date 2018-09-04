08a8751c9c3dcaf501433ed4c07896dee96e08cc
unexpectedness <simplertodescribethantogenerate@proton.me>
2018-09-04 17:48:46 +0200

    Bumped shuriken to 0.14.9 and arity to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [0.1.3] - 2018-09-04
+### Changed
+- Bump `shuriken` dependency to `0.14.9`.
+- Bump `arity` dependency to `0.2.0`.
+
 ## [0.1.2] - 2018-05-13
 ### Changed
 - Add a new arity to `make-widget-async` to provide a different widget shape.
--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject weaving "0.1.2"
+(defproject weaving "0.1.3"
   :description "Combinators to weave Clojure functions together"
   :url "https://github.com/unexpectedness/weaving"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [arity "0.1.0"]]
+                 [arity "0.2.0"]]
   :profiles {:test {:dependencies
-                    [[net.clojars.unexpectedness/shuriken "0.13.30"]]}
+                    [[net.clojars.unexpectedness/shuriken "0.14.9"]]}
              :dev  {:dependencies
-                    [[net.clojars.unexpectedness/shuriken "0.13.30"]
+                    [[net.clojars.unexpectedness/shuriken "0.14.9"]
                      [codox-theme-rdash "0.1.2"]]}}
   :plugins [[lein-codox "0.10.3"]]
   :codox {:source-uri "https://github.com/unexpectedness/weaving/" \
