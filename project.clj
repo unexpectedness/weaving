8c17ad216ef57ef0e982092571332db165d67cb1
unexpectedness <simplertodescribethantogenerate@proton.me>
2024-03-03 17:19:13 +0100

    Fix deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weaving "0.1.5"
+(defproject net.clojars.unexpectedness/weaving "0.1.5.1"
   :description "Combinators to weave Clojure functions together"
   :url "https://github.com/unexpectedness/weaving"
   :license {:name "Eclipse Public License"
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [arity "0.2.0"]]
   :profiles {:test {:dependencies
-                    [[net.clojars.unexpectedness/shuriken "0.14.28"]]}
+                    [[net.clojars.unexpectedness/shuriken "0.14.52"]]}
              :dev  {:dependencies
-                    [[net.clojars.unexpectedness/shuriken "0.14.28"]
+                    [[net.clojars.unexpectedness/shuriken "0.14.52"]
                      [codox-theme-rdash "0.1.2"]]}}
   :plugins [[lein-codox "0.10.3"]]
   :codox {:source-uri "https://github.com/unexpectedness/weaving/" \
